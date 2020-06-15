06ecb84d39fdfcc24de70b9c6d4134531103bb89
Tzahi-14 <tzahidavid014@gmail.com>
2020-06-15 17:01:46 +0300

    add some files

---
+++ Desktop/Code/bootcamp/personal-exercises/week-1/projectjs/main.js
@@ -0,0 +1,42 @@
+/*console.log("Hello, world!")
+let shoppingCartItems
+console.log (shoppingCartItems)
+console.log("51" !== 52)*/
+
+// const maxCapacity = 14
+// console.log (maxCapacity)
+// let surfTime = true
+// console.log (surfTime)
+// let bestStudent 
+// console.log (bestStudent)
+// const purposeInLife 
+// console.log (purposeInLife)
+
+let newString = "programming "
+newString= newString+ "is the best thing ever"
+console.log(newString)
+
+let password = 22;
+let confirmPassword = 24;
+console.log(password===confirmPassword)
+
+let num = 423
+let num3 = 12
+num = num *num3
+let num2 = 802
+let num4 = 2
+num2 = num2/2
+console.log((6+5)*3)
+
+console.log (!!true)
+
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+b = c
+c = a
+a = b
+console.log(a,b,c)
