6d84a3984032c173ff63d0776a63ae640e0632a1
Tzahi-14 <tzahidavid014@gmail.com>
2020-06-16 09:02:58 +0300

    JS task

---
+++ Conditional/main.js
@@ -0,0 +1,107 @@
+//Exercise 1
+
+// let numChildren = prompt ("How many children you have?");
+// let isCareful = prompt ("By true or false , are you careful user ") ;
+// if (isCareful){
+//     console.log(numChildren);
+// }
+// else {
+//     console.log (numChildren - 1);
+// }
+
+//Exercise 2
+
+// let silverwareCount = 5
+// if (silverwareCount % 2 === 1){
+//     console.log ("there is something missing")
+// }
+
+//Exercise 3
+
+// let performance = "stellar"
+// let salary = 10000
+// const goodBonus = 1000
+// const stellarBonus = 3000
+// if (performance === "stellar"){
+//     salary += stellarBonus
+//     console.log (salary)
+// }
+// else {
+//     salary += goodBonus
+//     console.log (salary)
+// }
+
+//Exercise 4
+// const isVIP = false
+// let cash = 500
+// if (isVIP || cash >300) {
+//     console.log ("Hello dear friend, have fun")
+// }
+// else {
+//     console.log ("go away and don't come back")
+// }
+
+//Exercise 5
+
+// const a = 3
+// let b = 2
+// let c = 0 || 12
+// let d
+
+// b = c
+// b++
+// if(d){
+//   b = a
+// }
+
+// d = a + (b * c)
+// d++
+// b += 2
+//   console.log (a,b,c,d)
+
+//Exercise 6
+// const gender = null
+// let profession = "business"
+// if (gender === "male"){
+//     console.log ("business" + "man")
+// }
+// else if(gender==="female") {
+//     console.log ("business" + "woman")
+// }
+// else {
+//     console.log ("business" + "person")
+// }
+
+// const gender = null
+// let profession = "business"
+// if (gender != "male" && gender != "female"){
+//     console.log ("business" + "person")
+// }
+// else if(gender != "male" && gender != "null") {
+//     console.log ("business" + "woman")
+// }
+// else {
+//     console.log ("business" + "man")
+// }
+
+// let boughtTesla = true
+// const yearOfTeslaPurchase = 2014
+// let isUSCitizen = true
+// let currentYear = 2018
+// let longAgo
+
+// if (boughtTesla === true && isUSCitizen === true){
+//     longAgo = currentYear - yearOfTeslaPurchase
+//     if (longAgo >= 4)
+//     console.log ("Would you like an upgrade?")
+//     else {
+//         console.log ("are you satisfied with the car?")
+//     }
+// }
+// else if (boughtTesla === true && isUSCitizen === false) {
+//     console.log ("would you like to move to US?")
+// }
+// else {
+//     console.log ("would you like to buy one?")
+// }
+
--- projectjs/main.js
+++ projectjs/main.js
@@ -1,42 +1,63 @@
-/*console.log("Hello, world!")
-let shoppingCartItems
-console.log (shoppingCartItems)
-console.log("51" !== 52)*/
+let enoughMoneyInBank = false
+let hasJob = true
+if(enoughMoneyInBank){
+    //buy a gift
+}else {
+    if (hasJob){
+        console.log ("go to work now!")
+    }
+    else{
+        console.log ("send your CV, and find your self a job")
+    }
+    
+}
 
-// const maxCapacity = 14
-// console.log (maxCapacity)
-// let surfTime = true
-// console.log (surfTime)
-// let bestStudent 
-// console.log (bestStudent)
-// const purposeInLife 
-// console.log (purposeInLife)
+// let username = prompt("What is your username?")
 
-let newString = "programming "
-newString= newString+ "is the best thing ever"
-console.log(newString)
 
-let password = 22;
-let confirmPassword = 24;
-console.log(password===confirmPassword)
 
-let num = 423
-let num3 = 12
-num = num *num3
-let num2 = 802
-let num4 = 2
-num2 = num2/2
-console.log((6+5)*3)
 
-console.log (!!true)
 
-let a = 3
-a = 4
-let c = 0
-let b = a
-b = 2
-a = b
-b = c
-c = a
-a = b
-console.log(a,b,c)
+
+// /*console.log("Hello, world!")
+// let shoppingCartItems
+// console.log (shoppingCartItems)
+// console.log("51" !== 52)*/
+
+// // const maxCapacity = 14
+// // console.log (maxCapacity)
+// // let surfTime = true
+// // console.log (surfTime)
+// // let bestStudent 
+// // console.log (bestStudent)
+// // const purposeInLife 
+// // console.log (purposeInLife)
+
+// let newString = "programming "
+// newString= newString+ "is the best thing ever"
+// console.log(newString)
+
+// let password = 22;
+// let confirmPassword = 24;
+// console.log(password===confirmPassword)
+
+// let num = 423
+// let num3 = 12
+// num = num *num3
+// let num2 = 802
+// let num4 = 2
+// num2 = num2/2
+// console.log((6+5)*3)
+
+// console.log (!!true)
+
+// let a = 3
+// a = 4
+// let c = 0
+// let b = a
+// b = 2
+// a = b
+// b = c
+// c = a
+// a = b
+// console.log(a,b,c)
